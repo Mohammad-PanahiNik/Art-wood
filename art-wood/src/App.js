6f6601005c419306dfc2950773cba7b1bdf377de
Ali Kiadarbandsari <aliakd1122@gmail.com>
2024-02-23 14:56:30 +0330

    footer

--- art-wood/src/App.js
+++ art-wood/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import Header from "./Components/Header/header.jsx";
+import Order from "./Components/Order/order.jsx";
+import Contract from "./Components/Contract/contract.jsx";
+import Footer from "./Components/Footer/footer.jsx";
+
 
 function App() {
   return (
-    <Header/>
+    <>
+        <Header/>
+        <Order/>
+        <Contract/>
+        <Footer/>
+    </>
+
   )
 }
 
