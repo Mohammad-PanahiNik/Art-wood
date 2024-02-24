850704cc0d2ca982631fff3cf49e1cc7e4cbe08a
Mohammad-PanahiNik <125298901+Mohammad-PanahiNik@users.noreply.github.com>
2024-02-24 17:58:59 +0330

    add about us section

--- art-wood/src/App.js
+++ art-wood/src/App.js
@@ -4,6 +4,7 @@ import Introduction from "./Components/Introduction/introduction.jsx";
 import Order from "./Components/Order/order.jsx";
 import Contract from "./Components/Contract/contract.jsx";
 import Footer from "./Components/Footer/footer.jsx";
+import AboutUs from "./Components/AboutUs/about-us.jsx";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Introduction/>
       <Order/>
       <Contract/>
+      <AboutUs/>
       <Footer/>
     </>
 
